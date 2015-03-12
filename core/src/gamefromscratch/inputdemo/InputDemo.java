9a62b3ac8600629d3af27f5d2bb6df4bb9ba445b
emp <acclist@hotmail.com>
2015-03-12 17:18:57 +0200

    Added Multitouch example

--- core/src/gamefromscratch/inputdemo/InputDemo.java
+++ core/src/gamefromscratch/inputdemo/InputDemo.java
@@ -1,27 +1,52 @@
 package gamefromscratch.inputdemo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class InputDemo extends ApplicationAdapter {
+public class InputDemo extends ApplicationAdapter implements InputProcessor{
 	SpriteBatch batch;
-	Texture texture;
-	Sprite sprite;
+	BitmapFont font;
+	String message = "Touch something already!";
+	int w,h;
+	
+	class TouchInfo {
+		public float touchX = 0;
+		public float touchY = 0;
+		public boolean touched = false;
+	}
+	
+	private Map<Integer, TouchInfo> touches = new HashMap<Integer, TouchInfo>();
 	
 	@Override
 	public void create () {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
+		font = new BitmapFont(Gdx.files.internal("data/arial-15.fnt"), false);
+		font.setColor(Color.RED);
 		
-		texture = new Texture(Gdx.files.internal("data/0001.pgn"));
-		sprite = new Sprite(texture);
-		sprite.setPosition(w/2 - sprite.getWidth()/2, h/2 - sprite.getHeight()/2);
+		Gdx.input.setInputProcessor(this);
+		for (int i = 0; i < 5; i++) {
+			touches.put(i, new TouchInfo());
+		}
+	}
+	
+	@Override
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 
 	@Override
@@ -29,22 +54,39 @@ public class InputDemo extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateX(-1f);
-			else
-				sprite.translateX(-10f);
-		}
-		
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT))
-				sprite.translateX(1f);
-			else
-				sprite.translateX(10f);
-		}
 		
 		batch.begin();
-		sprite.draw(batch);
+		
+		message = "";
+		for (int i = 0; i < 5; i++) {
+			if (touches.get(i).touched)
+				message += "Finger: " + Integer.toString(i) + " touch at: " + Float.toString(touches.get(i).touchX) + ","+ Float.toString(touches.get(i).touchY) + "\n";
+		}
+		TextBounds tb = font.getBounds(message);
+		float x = w / 2 - tb.width / 2;
+		float y = h / 2 + tb.height / 2;
+		font.drawMultiLine(batch, message, x, y);
+		
 		batch.end();
+	}
+	
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		if (pointer < 5) {
+			touches.get(pointer).touchX = 0;
+			touches.get(pointer).touchY = 0;
+			touches.get(pointer).touched = false;
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if (pointer < 5) {
+			touches.get(pointer).touchX = screenX;
+			touches.get(pointer).touchY = screenY;
+			touches.get(pointer).touched = true;
+		}
+		return true;
 	}
 }
