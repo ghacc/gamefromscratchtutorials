06473686a40f7791c5595ae4a8d5a78f8e2ea30b
emp <acclist@hotmail.com>
2015-03-12 23:12:11 +0200

    Filled unimplemented methods

--- core/src/gamefromscratch/inputdemo/InputDemo.java
+++ core/src/gamefromscratch/inputdemo/InputDemo.java
@@ -89,4 +89,40 @@ public class InputDemo extends ApplicationAdapter implements InputProcessor{
 		}
 		return true;
 	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
