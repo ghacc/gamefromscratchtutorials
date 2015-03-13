235b20bfa1d7a3767843a922ddad7c44cf344c4d
emp <acclist@hotmail.com>
2015-03-14 01:00:21 +0200

    Touch gestures branch initial commit

--- core/src/gamefromscratch/inputdemo/InputDemo.java
+++ core/src/gamefromscratch/inputdemo/InputDemo.java
@@ -2,26 +2,31 @@ package gamefromscratch.inputdemo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
 
-public class InputDemo extends ApplicationAdapter {
+public class InputDemo extends ApplicationAdapter implements GestureListener {
 	SpriteBatch batch;
-	Texture texture;
-	Sprite sprite;
+	BitmapFont font;
+	String message = "No gesture performed yet";
+	int w, h;
 	
 	@Override
 	public void create () {
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(Color.RED);
 		
-		texture = new Texture(Gdx.files.internal("data/0001.pgn"));
-		sprite = new Sprite(texture);
-		sprite.setPosition(w/2 - sprite.getWidth()/2, h/2 - sprite.getHeight()/2);
+		GestureDetector gd = new GestureDetector(this);
+		Gdx.input.setInputProcessor(gd);
 	}
 
 	@Override
@@ -29,22 +34,67 @@ public class InputDemo extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateX(-1f);
-			else
-				sprite.translateX(-10f);
-		}
-		
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT))
-				sprite.translateX(1f);
-			else
-				sprite.translateX(10f);
-		}
-		
 		batch.begin();
-		sprite.draw(batch);
+		
+		TextBounds tb = font.getBounds(message);
+		float x = w / 2 - tb.width / 2;
+		float y = h / 2 - tb.height / 2;
+		
+		font.drawMultiLine(batch, message, x, y);
+		
 		batch.end();
+	}
+
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		message = "Tap performed, finger " + Integer.toString(button);
+		return true;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		message = "Long press performed";
+		return true;
+	}
+
+	// aka flick
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		message = "Fling performed, velocity: " + Float.toString(velocityX) + "," + Float.toString(velocityY);
+		return true;
+	}
+	
+	// one finger hold and slide
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		message = "Pan performed, delta: " + Float.toString(deltaX) + "," + Float.toString(deltaY);
+		return true;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	// two fingers moving apart (zoom out)
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		message = "Zoom performed, initial distance: " + Float.toString(initialDistance) + ", distance: " + Float.toString(distance);
+		return true;
+	}
+	
+	// two fingers moving together (zoom in)
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
+			Vector2 pointer1, Vector2 pointer2) {
+		message = "Pinch performed";
+		return true;
 	}
 }
