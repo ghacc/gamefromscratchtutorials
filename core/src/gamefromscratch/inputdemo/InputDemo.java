65275a2e48bc01c03f27b1665701337e48e04e67
emp <acclist@hotmail.com>
2015-03-20 03:03:57 +0200

    Initial commit for part 2 of scene2D tutorial
    tip: it's about actions

--- core/src/gamefromscratch/inputdemo/InputDemo.java
+++ core/src/gamefromscratch/inputdemo/InputDemo.java
@@ -5,23 +5,63 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 public class InputDemo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture texture;
-	Sprite sprite;
+	
+	public class MyActor extends Actor {
+		Texture texture = new Texture(Gdx.files.internal("data/jet.png"));
+		public boolean started = false;
+		
+		public MyActor() {
+			setBounds(getX(), getY(), texture.getWidth(), texture.getHeight());
+		}
+		
+		@Override
+		public void draw(Batch batch, float parentAlpha) {
+			batch.draw(texture, this.getX(), this.getY(), this.getOriginX(), this.getOriginY(), this.getWidth(), this.getHeight(),
+					this.getScaleX(), this.getScaleY(), this.getRotation(), 0, 0, texture.getWidth(), texture.getHeight(), false, false);
+		}
+	}
+	
+	private Stage stage;
 	
 	@Override
 	public void create () {
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		batch = new SpriteBatch();
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
 		
-		texture = new Texture(Gdx.files.internal("data/0001.pgn"));
-		sprite = new Sprite(texture);
-		sprite.setPosition(w/2 - sprite.getWidth()/2, h/2 - sprite.getHeight()/2);
+		MyActor actor = new MyActor();
+		
+		SequenceAction sequenceAction = new SequenceAction();
+		
+		MoveToAction moveAction = new MoveToAction();
+		moveAction.setPosition(300f, 0f);
+		moveAction.setDuration(10f);
+		
+		RotateToAction rotateAction = new RotateToAction();
+		rotateAction.setRotation(90f);
+		rotateAction.setDuration(5f);
+		
+		ScaleToAction scaleAction = new ScaleToAction();
+		scaleAction.setScale(0.5f);
+		scaleAction.setDuration(5f);
+		
+		sequenceAction.addAction(moveAction);
+		sequenceAction.addAction(rotateAction);
+		sequenceAction.addAction(scaleAction);
+		
+		actor.addAction(sequenceAction);
+		
+		stage.addActor(actor);
 	}
 
 	@Override
@@ -29,22 +69,8 @@ public class InputDemo extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateX(-1f);
-			else
-				sprite.translateX(-10f);
-		}
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 		
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT))
-				sprite.translateX(1f);
-			else
-				sprite.translateX(10f);
-		}
-		
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
 	}
 }
